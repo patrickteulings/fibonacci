b526c9872a654410f52d0592f0dbee1358d319d6
Patrick Teulings <patrickteulings@gmail.com>
2020-03-03 00:06:09 +0100

    Minor Cleanup
    Removed unwanted routes, files and components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <nav id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
       <router-link to="/fibonacci">Fibonacci Grid</router-link>
-    </div>
+    </nav>
     <router-view/>
   </div>
 </template>
